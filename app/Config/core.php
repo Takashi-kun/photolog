1f5dbc927b343458f3f11be11b8d37c77e98bac3
Takashi Honda <takashi.0628.honda@gmail.com>
2013-11-10 01:54:31 +0900

    fix notice

--- app/Config/core.php
+++ app/Config/core.php
@@ -194,12 +194,14 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+ //'photolog'のsha1 hash
+	Configure::write('Security.salt', '508162b954e7535c91c2fc79b43e94d7266ed3ca');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+ //'photolog' => 16hex => 10hex
+	Configure::write('Security.cipherSeed', '8099846475702104000');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
